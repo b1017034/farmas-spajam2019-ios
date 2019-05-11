092855e23d5190547ec1ab30eb0db7ecd9287ff8
daisuke sato <satodsuke@gmail.com>
2019-05-12 00:02:22 +0900

    湯気で火の強さを表現

--- sajam2019_iod/sajam2019_iod/Onsen/OnsenViewController.swift
+++ sajam2019_iod/sajam2019_iod/Onsen/OnsenViewController.swift
@@ -8,14 +8,33 @@
 import UIKit
 
 class OnsenViewController: UIViewController {
-
+    
+    var fire: Int = 2
+    
+    @IBOutlet weak var smokeImage: UIImageView!
+    
+    var img1 = UIImage(named:"IMG_2614.PNG")!
+    var img2 = UIImage(named:"IMG_2615.PNG")!
+    var img3 = UIImage(named:"IMG_2616.PNG")!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+       if fire < 1 {
+            smokeImage.image = img1
+        } else if fire < 2 {
+            smokeImage.image = img2
+        } else {
+            smokeImage.image = img3
+        }
     }
     
     @IBAction func postButton(_ sender: Any) {
+        /*let storyboard: UIStoryboard = UIStoryboard(name: "Post", bundle: nil)
+        let vc = storyboard.instantiateViewController(withIdentifier: "postViewController") as! PostViewController
+        //self.show(vc, sender: self)
+        self.present(vc, animated: true, completion: nil)*/
     }
     @IBAction func mypageButton(_ sender: Any) {
     }
