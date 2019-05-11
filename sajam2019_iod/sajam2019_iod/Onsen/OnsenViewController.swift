f77f9abfa673229076a1d0303e8356a700743ee3
中村　朝陽 <>
2019-05-11 16:29:01 +0900

    雛形作り

---
+++ sajam2019_iod/sajam2019_iod/Onsen/OnsenViewController.swift
@@ -0,0 +1,29 @@
+//
+//  OnsenViewController.swift
+//  
+//
+//  Created by 中村　朝陽 on 2019/05/11.
+//
+
+import UIKit
+
+class OnsenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ sajam2019_iod/sajam2019_iod/Post/PostViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PostViewController.swift
+//  sajam2019_iod
+//
+//  Created by 中村　朝陽 on 2019/05/11.
+//  Copyright © 2019年 asahi. All rights reserved.
+//
+
+import UIKit
+
+class PostViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
