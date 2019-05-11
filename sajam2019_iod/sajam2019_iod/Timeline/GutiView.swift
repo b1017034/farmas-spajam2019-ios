c553ba3249154d073721c3fbe5f87dc5df3f7325
中村　朝陽 <>
2019-05-11 18:09:01 +0900

    xib作成

---
+++ sajam2019_iod/sajam2019_iod/Timeline/GutiView.swift
@@ -0,0 +1,21 @@
+//
+//  GutiView.swift
+//  sajam2019_iod
+//
+//  Created by 中村　朝陽 on 2019/05/11.
+//  Copyright © 2019年 asahi. All rights reserved.
+//
+
+import UIKit
+
+class GutiView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
