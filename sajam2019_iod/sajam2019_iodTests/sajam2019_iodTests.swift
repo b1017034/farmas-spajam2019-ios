b3cfc4d777fd1aa3287871fb334ba54baa8a391c
中村　朝陽 <>
2019-05-11 15:40:24 +0900

    first commit

---
+++ sajam2019_iod/sajam2019_iodTests/sajam2019_iodTests.swift
@@ -0,0 +1,34 @@
+//
+//  sajam2019_iodTests.swift
+//  sajam2019_iodTests
+//
+//  Created by 中村　朝陽 on 2019/05/10.
+//  Copyright © 2019年 asahi. All rights reserved.
+//
+
+import XCTest
+@testable import sajam2019_iod
+
+class sajam2019_iodTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
