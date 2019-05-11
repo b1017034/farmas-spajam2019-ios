9137a9536c12f26c20e761f8fc07ed65af072400
daisuke sato <satodsuke@gmail.com>
2019-05-12 04:49:34 +0900

    条件分岐の記述

--- sajam2019_iod/sajam2019_iod/Onsen/OnsenViewController.swift
+++ sajam2019_iod/sajam2019_iod/Onsen/OnsenViewController.swift
@@ -8,14 +8,38 @@
 import UIKit
 
 class OnsenViewController: UIViewController {
+    
+    var fire: Int = 2
 
+    @IBOutlet weak var onsenImages: UIImageView!
+    
+    var img1 = UIImage(named:"IMG_2618.PNG")!
+    var img2 = UIImage(named:"IMG_2627.PNG")!
+    var img3 = UIImage(named:"IMG_2624.PNG")!
+    var img4 = UIImage(named:"IMG_2626.PNG")!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        if fire == 0 {
+            onsenImages.image = img1
+        } else if fire == 1 {
+            onsenImages.image = img2
+        } else if fire == 2 {
+            onsenImages.image = img3
+        } else {
+            onsenImages.image = img4
+        }
     }
     
-
+    @IBAction func postButton(_ sender: Any) {
+    }
+    @IBAction func mypageButton(_ sender: Any) {
+    }
+    @IBAction func timelineButton(_ sender: Any) {
+    }
+    
     /*
     // MARK: - Navigation
 
